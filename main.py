cbdbddb69f6c16725c8ae449f090747353eeac35
KyoungHwan <xzt______@naver.com>
2022-03-16 23:55:03 +0900

    CRUD Test complete

--- main.py
+++ main.py
@@ -44,6 +44,34 @@ async def get_user(user_id: None, *, session: AsyncSession = Depends(get_session
     return instance
 
 
+@app.patch('/update_user/{user_id}', response_model=User)
+async def update_user(data: User, user_id: None, *, session: AsyncSession = Depends(get_session)):
+    instance = await session.get(User, user_id)
+    if not instance:
+        raise HTTPException(status_code=404, detail=f"This User is not exist.")
+    update_data = data.dict(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(instance, key, value)
+
+    session.add(instance)
+    await session.commit()
+    await session.refresh(instance)
+    return instance
+
+
+@app.delete('/delete_user/{user_id}', response_model=User)
+async def delete_user(user_id: None, *, session: AsyncSession = Depends(get_session)):
+    instance = await session.get(User, user_id)
+    if not instance:
+        raise HTTPException(status_code=404, detail=f"This User is not exist.")
+
+    await session.delete(instance)
+    await session.commit()
+
+    return user_id
+
+
 if __name__ == "__main__":
     conf = Config(".env")
     uvicorn.run(
